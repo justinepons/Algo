e5647984513633527733bda1b495b6ecb4c8c17f
adruel <adrien.ruel@numericable.fr>
2016-12-30 12:46:44 +0100

    Add files via upload

--- MainWindow.java
+++ MainWindow.java
@@ -3,18 +3,23 @@ import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -26,6 +31,15 @@ public class MainWindow extends Application {
     Date deb;
     Date fini;
    static List <Long> intervalle;
+   
+   private Node getNodeFromGridPane(GridPane gridPane, int col, int row) {
+    for (Node node : gridPane.getChildren()) {
+        if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
+            return node;
+        }
+    }
+    return null;
+}
     
     public static List <Long>  getintervalle(){
         return intervalle;
@@ -71,16 +85,27 @@ public class MainWindow extends Application {
 		pane.add(lbl_inter,0,3);
                 TextField txt_inter = new TextField();
 		pane.add(txt_inter,1,3);
-        
+                RadioButton rb1 = new RadioButton();
+                pane.add(rb1,2,3);
                 
+                Label lbl_inter2 = new Label("Intervalles de temps définis manuellement");
+		pane.add(lbl_inter2,0,4);
+                TextField txt_inter2 = new TextField();
+		pane.add(txt_inter2,1,4);
+                RadioButton rb2 = new RadioButton();
+                pane.add(rb2,2,4);
+                
+                final ToggleGroup group = new ToggleGroup();
+                rb1.setToggleGroup(group);
+                rb2.setToggleGroup(group);
                 
                 Label lbl_sauv = new Label("Entrez le fichier de sauvegarde");
-		pane.add(lbl_sauv,0,4);
+		pane.add(lbl_sauv,0,5);
 		TextField txt_sauv = new TextField();
-		pane.add(txt_sauv,1,4);
+		pane.add(txt_sauv,1,5);
                 Button filechsv= new Button("File Chooser sv");
                 filechsv.setText("Fichier");
-                pane.add(filechsv,2,4);
+                pane.add(filechsv,2,5);
                 
                 filechsv.setOnAction(new EventHandler<ActionEvent>() {
                         @Override
@@ -102,7 +127,7 @@ public class MainWindow extends Application {
                 
                 Button valider = new Button("Valider");
                     valider.setText("Valider");
-                    pane.add(valider,1,5);
+                    pane.add(valider,1,6);
                     
                     valider.setOnAction(new EventHandler<ActionEvent>() {
                         @Override
@@ -155,9 +180,10 @@ public class MainWindow extends Application {
                                         Projet.alimenter_stock(file,deb,fini);
 
                                 //} 
-                                
+                                if(rb1.isSelected()==true){
+                                 
                                 intervalle = Projet.intervalle_taille_egale(Integer.parseInt(txt_inter.getText()));
-                               
+                                
                                 Projet.sauvegarder_stock(txt_sauv.getText());
                                  Stage dialogStage = new Stage();
                                    
@@ -167,6 +193,107 @@ public class MainWindow extends Application {
 
                                     dialogStage.setScene(new Scene(vbox));
                                     dialogStage.show();
+                                }else{
+                                   if(rb2.isSelected() == true){
+                                       Stage dialogStage = new Stage();
+                                       int n;
+                                       
+                                       n = Integer.parseInt(txt_inter2.getText());
+                                       TextField[] tf = new TextField[n];
+                                       GridPane pane2 = new GridPane();
+                                       
+                                       
+
+                                        SimpleDateFormat formatter = new SimpleDateFormat("dd MM yyyy");
+                                        
+
+
+                                       Label lbl11 = new Label("Debut de l'étude");
+                                       pane2.add(lbl11, 0, 0);
+                                       Label lbl12 = new Label(formatter.format(deb));
+                                       pane2.add(lbl12, 1, 0);
+                                       
+                                       for(int i=1;i<n;i++){
+                                           Label lbl_dyn = new Label("Fin de l'intervalle (format dd mm yyyy) " + i);
+                                           TextField txt_dyn = new TextField();
+                                           pane2.add(lbl_dyn,0,i);
+                                           pane2.add(txt_dyn,1,i);
+                                           
+                                           
+                                       }
+                                       
+                                       Label lbln1 = new Label("Fin de l'étude");
+                                       pane2.add(lbln1, 0, n);
+                                       Label lbln2 = new Label(formatter.format(fini));
+                                       pane2.add(lbln2, 1, n);
+                                       
+                                       
+                                       Button btn_inter2 = new Button();
+                                       btn_inter2.setText("Valider");
+                                       pane2.add(btn_inter2,1,n+1);
+                                       
+                                       btn_inter2.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent event) {
+                                    intervalle = new ArrayList();
+                            try {
+                                intervalle.add(formatter.parse(lbl12.getText()).getTime());
+                            } catch (ParseException ex) {
+                                Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                                    for(int j=1;j<n;j++){
+                                        Node noeu = getNodeFromGridPane(pane2,1,j);
+                                        if(noeu instanceof TextField){
+                                            String chaine =  ((TextField)noeu).getText();
+                                            try {
+                                                intervalle.add(formatter.parse(chaine).getTime());
+                                            } catch (ParseException ex) {
+                                                Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+                                            }
+                                        }
+                                    }
+                            /*try {
+                                intervalle.add(formatter.parse(lbln2.getText()).getTime());
+                            } catch (ParseException ex) {
+                                Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+                            }*/
+                            
+                                
+                            try {        
+                                Projet.sauvegarder_stock(txt_sauv.getText());
+                                Stage dialogStage = new Stage();
+                                   
+
+                                    VBox vbox = new VBox(new Text("Fichier Sauvegarder avec succès"));
+                                  
+
+                                    dialogStage.setScene(new Scene(vbox));
+                                    dialogStage.show();
+                                
+                            } catch (IOException ex) {
+                                Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            dialogStage.hide();
+                            }
+                                       });
+                                       
+                                       
+                                       
+                                       dialogStage.setScene(new Scene(pane2));
+                                    dialogStage.show();
+                                       
+                                       
+                                   }else{
+                                       Stage dialogStage = new Stage();
+                                   
+
+                                    VBox vbox = new VBox(new Text("Veuillez choisir le type de découpage temporel"));
+                                  
+
+                                    dialogStage.setScene(new Scene(vbox));
+                                    dialogStage.show();
+                                   }
+                                }
                                 }
                                     }
                                 
